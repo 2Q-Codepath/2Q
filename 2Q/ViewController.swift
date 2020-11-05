c6f152e68094bb7f8696da88753cfd1f6c1672fc
Sterling <sterlingsgamble@gmail.com>
2020-11-05 11:42:54 -0800

    initial files

---
+++ 2Q/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2Q
+//
+//  Created by Sterling Gamble on 11/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-2QTests/Pods-2QTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_2QTests : NSObject
+@end
+@implementation PodsDummy_Pods_2QTests
+@end
