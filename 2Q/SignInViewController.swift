be5c42b37499488f22419b8e55ef24f74581531d
Sterling <sterlingsgamble@gmail.com>
2020-11-05 22:19:30 -0800

    sign in

---
+++ 2Q/SignInViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SignInViewController.swift
+//  2Q
+//
+//  Created by Sterling Gamble on 11/5/20.
+//
+
+import UIKit
+import Parse
+
+class SignInViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "signinSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
