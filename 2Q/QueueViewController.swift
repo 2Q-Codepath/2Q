589ba8d6eb07b4299ed4e4fcaa4b84640c0acc83
Jon-Jon <j2naraja@gmail.com>
2020-11-16 14:05:55 -0800

    Created current/saved queue page

---
+++ 2Q/QueueViewController.swift
@@ -0,0 +1,31 @@
+//
+//  QueueViewController.swift
+//  2Q
+//
+//  Created by Jonathan Naraja on 11/16/20.
+//
+
+import UIKit
+
+class QueueViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
